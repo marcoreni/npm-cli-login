1976618717c03aca2dd2734e446759142845b378
Marco Reni <marco.reni@piksel.com>
2018-10-02 16:03:03 +0200

    Eslinting & nodejs > 6

--- bin/npm-cli-login.js
+++ bin/npm-cli-login.js
@@ -1,33 +1,34 @@
 #!/usr/bin/env node
+const mainModule = require('../lib');
 
-var getArg = function (marker, isBoolean) {
-    var pos = process.argv.indexOf(marker);
-    return (pos === -1) ? -1 : (isBoolean ? pos : pos + 1);
+const getArg = (marker, isBoolean) => {
+  const pos = process.argv.indexOf(marker);
+  return (pos === -1) ? -1 : (isBoolean ? pos : pos + 1);
 };
 
-var login = function () {
-    var found = getArg('-u', false);
-    var user = (found === -1) ? process.env.NPM_USER : process.argv[found];
+const login = () => {
+  let found = getArg('-u', false);
+  const user = (found === -1) ? process.env.NPM_USER : process.argv[found];
 
-    found = getArg('-p', false);
-    var pass = (found === -1) ? process.env.NPM_PASS : process.argv[found];
+  found = getArg('-p', false);
+  const pass = (found === -1) ? process.env.NPM_PASS : process.argv[found];
 
-    found = getArg('-e', false);
-    var email = (found === -1) ? process.env.NPM_EMAIL : process.argv[found];
+  found = getArg('-e', false);
+  const email = (found === -1) ? process.env.NPM_EMAIL : process.argv[found];
 
-    found = getArg('-r', false);
-    var registry = (found === -1) ? process.env.NPM_REGISTRY : process.argv[found];
+  found = getArg('-r', false);
+  const registry = (found === -1) ? process.env.NPM_REGISTRY : process.argv[found];
 
-    found = getArg('-s', false);
-    var scope = (found === -1) ? process.env.NPM_SCOPE : process.argv[found];
+  found = getArg('-s', false);
+  const scope = (found === -1) ? process.env.NPM_SCOPE : process.argv[found];
 
-    found = getArg('--config-path', false);
-    var configPath = (found === -1) ? process.env.NPM_RC_PATH : process.argv[found];
+  found = getArg('--config-path', false);
+  const configPath = (found === -1) ? process.env.NPM_RC_PATH : process.argv[found];
 
-    found = getArg('--quotes', true);
-    var quotes = (found === -1) ? false : true;
+  found = getArg('--quotes', true);
+  const quotes = found !== -1;
 
-    require('../')(user, pass, email, registry, scope, quotes, configPath);
+  mainModule(user, pass, email, registry, scope, quotes, configPath);
 };
 
 login();
